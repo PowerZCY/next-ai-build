a712a228eaada018481a8d7a859a4a18f9153843
D8GER <xyb5to0zcy@gmail.com>
2025-05-14 23:59:23 +0800

    fix(locale): foot link with locale

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useTranslations } from 'next-intl'
+import { useTranslations, useLocale } from 'next-intl'
 import Link from "next/link";
 import { SiteIcon } from '@/components/global-icon';
 import { globalLucideIcons as icons } from '@/components/global-icon';
@@ -10,6 +10,7 @@ import { showBanner } from '@/lib/appConfig';
 export function Footer() {
   const t = useTranslations('home');
   const tFooter = useTranslations('footer');
+  const locale = useLocale();
 
   useEffect(() => {
     // 如果设置了banner, 就需要调节header的高度
@@ -43,15 +44,15 @@ export function Footer() {
           <div className="flex flex-col items-center text-center m-2 space-y-5">
             {/* Row 2.1: Legal Links */}
             <div className="flex items-center space-x-4 text-xs">
-              <Link href="/legal/terms" className="flex items-center space-x-1 hover:underline">
+              <Link href={`/${locale}/legal/terms`} className="flex items-center space-x-1 hover:underline">
                 <icons.ReceiptText className="h-3.5 w-3.5"/>
                 <span>{tFooter('terms', { defaultValue: 'Terms of Service' })}</span>
               </Link>
-              <Link href="/legal/privacy" className="flex items-center space-x-1 hover:underline">
+              <Link href={`/${locale}/legal/privacy`} className="flex items-center space-x-1 hover:underline">
                 <icons.ShieldUser className="h-3.5 w-3.5"/>
                 <span>{tFooter('privacy', { defaultValue: 'Privacy Policy' })}</span>
               </Link>
-              <Link href="/legal/dpa" className="flex items-center space-x-1 hover:underline">
+              <Link href={`/${locale}/legal/dpa`} className="flex items-center space-x-1 hover:underline">
                 <icons.DPA className="h-3.5 w-3.5"/>
                 <span>{tFooter('dpa', { defaultValue: 'Data Protection Agreement' })}</span>
               </Link>
@@ -68,48 +69,48 @@ export function Footer() {
           <div className="flex justify-end m-2 space-x-4">
             <div className="flex flex-col space-y-5">
               {/* Link 1 */}
-              <Link href="/docs/introduction/mdx-shiki" className="text-xs flex items-center space-x-1 hover:underline text-left">
+              <Link href={`/${locale}/docs/introduction/mdx-shiki`} className="text-xs flex items-center space-x-1 hover:underline text-left">
                 <icons.Highlighter className="h-3.5 w-3.5"/>
                 <span>Shiki</span>
               </Link>
               {/* Link 2 */}
-              <Link href="/docs/introduction/mdx-snippets" className="text-xs flex items-center space-x-1 hover:underline text-left">
+              <Link href={`/${locale}/docs/introduction/mdx-snippets`} className="text-xs flex items-center space-x-1 hover:underline text-left">
                 <icons.Snippets className="h-3.5 w-3.5"/>
                 <span>Snippets</span>
               </Link>
             </div>
             <div className="flex flex-col space-y-5">
               {/* Link 1 */}
-              <Link href="/docs/introduction/mdx-mermaid" className="text-xs flex items-center space-x-1 hover:underline text-left">
+              <Link href={`/${locale}/docs/introduction/mdx-mermaid`} className="text-xs flex items-center space-x-1 hover:underline text-left">
                 <icons.Mmd className="h-3.5 w-3.5"/>
                 <span>Mermaid</span>
               </Link>
               {/* Link 2 */}
-              <Link href="/docs/tool-manuals/d8ger-auto-code" className="text-xs flex items-center space-x-1 hover:underline text-left">
+              <Link href={`/${locale}/docs/tool-manuals/d8ger-auto-code`} className="text-xs flex items-center space-x-1 hover:underline text-left">
                 <icons.D8 className="h-3.5 w-3.5"/>
                 <span>D8gerAutoCode</span>
               </Link>
             </div>
             <div className="flex flex-col space-y-5">
               {/* Link 1 */}
-              <Link href="/docs/tool-manuals/macos-software" className="text-xs flex items-center space-x-1 hover:underline text-left">
+              <Link href={`/${locale}/docs/tool-manuals/macos-software`} className="text-xs flex items-center space-x-1 hover:underline text-left">
                 <icons.MAC className="h-3.5 w-3.5"/>
                 <span>Software</span>
               </Link>
               {/* Link 2 */}
-              <Link href="/docs/tool-manuals/easy-http" className="text-xs flex items-center space-x-1 hover:underline text-left">
+              <Link href={`/${locale}/docs/tool-manuals/easy-http`} className="text-xs flex items-center space-x-1 hover:underline text-left">
                 <icons.Iterm className="h-3.5 w-3.5"/>
                 <span>EasyHttp</span>
               </Link>
             </div>
             <div className="flex flex-col space-y-5">
               {/* Link 1 */}
-              <Link href="/docs/tool-manuals/git" className="text-xs flex items-center space-x-1 hover:underline text-left">
+              <Link href={`/${locale}/docs/tool-manuals/git`} className="text-xs flex items-center space-x-1 hover:underline text-left">
                 <icons.GitPullRequestArrow className="h-3.5 w-3.5"/>
                 <span>Git</span>
               </Link>
               {/* Link 2 */}
-              <Link href="/docs/tool-manuals/httpie" className="text-xs flex items-center space-x-1 hover:underline text-left">
+              <Link href={`/${locale}/docs/tool-manuals/httpie`} className="text-xs flex items-center space-x-1 hover:underline text-left">
                 <icons.Terminal className="h-3.5 w-3.5"/>
                 <span>HTTPie</span>
               </Link>
