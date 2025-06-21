fe6cd03a04735c7f901d17c2b1b1fd8faab7a5da
D8GER <xyb5to0zcy@gmail.com>
2025-06-21 21:42:12 +0800

    optimize(mdx): ImageGrid center style

--- src/components/mdx/imageGrid.tsx
+++ src/components/mdx/imageGrid.tsx
@@ -19,6 +19,7 @@ export function ImageGrid({
         gridTemplateColumns: 'repeat(2, 1fr)',
         gap: '16px',
         justifyItems: 'center',
+        alignItems: 'center', 
       }}
     >
       {images.map((img, idx) => (
