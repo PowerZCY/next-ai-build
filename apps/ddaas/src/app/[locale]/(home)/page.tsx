7a523882e5ab384bb4456544d8c4c3d0a361185a
D8GER <xyb5to0zcy@gmail.com>
2025-07-17 23:43:49 +0800

    optimize(config): static config diff with UI config

--- apps/ddaas/src/app/[locale]/(home)/page.tsx
+++ apps/ddaas/src/app/[locale]/(home)/page.tsx
@@ -1,6 +1,6 @@
 import { Hero } from "@/components/hero"
 import { Gallery, Usage, Features, Tips, FAQ, SeoContent, CTA, PricePlan } from "@third-ui/main"
-import { pricePlanConfig } from "@/lib/appConfig"
+import { pricePlanConfig } from "@/lib/price-config"
 
 export default function Home() {
   return (
--- apps/ddaas/src/lib/appConfig.ts
+++ apps/ddaas/src/lib/appConfig.ts
@@ -1,5 +1,4 @@
 import { createCommonAppConfig, createI18nHelpers, LOCALE_PRESETS } from "@lib/common-app-config";
-import { PricePlanAppConfig } from "@third-ui/main/price-plan";
 
 // 创建应用配置
 export const appConfig = {
@@ -11,16 +10,3 @@ export const { isSupportedLocale, getValidLocale, generatedLocales } = createI18
 
 // 便捷常量直接从 shortcuts 导出
 export const { iconColor, watermark, showBanner, clerkPageBanner, clerkAuthInModal, placeHolderImage } = appConfig.shortcuts;
-
-export const pricePlanConfig: PricePlanAppConfig = {
-  billingOptions: [
-    { key: 'monthly', discount: 0 },
-    { key: 'yearly', discount: 0.20 }
-  ],
-  prices: {
-    free: 'Free',
-    premium: 10,
-    ultimate: 20,
-  },
-  minPlanFeaturesCount: 4
-}
