e1b04772b307e8ba46111932fafc7e1c8db82436
D8GER <xyb5to0zcy@gmail.com>
2025-07-22 03:02:35 +0800

    rollback(createApp): diaomao as a SINGLE project

--- apps/diaomao/src/app/[locale]/loading.tsx
+++
@@ -1,4 +0,0 @@
-'use client';
-import { Loading } from '@third-ui/main/loading';
-
-export default Loading;
--- apps/diaomao/src/lib/site-config.ts
+++
@@ -1,3 +0,0 @@
-import { createSiteIcon } from "@base-ui/components/server";
-
-export const SiteIcon = createSiteIcon('Pi');
